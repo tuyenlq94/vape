fd3d87d0873c562157cdce44d37433adecbe3bd9
tuyenlq94 <tuyenicolor@gmail.com>
2022-11-24 14:24:00 +0700

    fix banner home

--- template-parts/home/banner.php
+++ template-parts/home/banner.php
@@ -4,18 +4,10 @@ $banners = rwmb_meta( 'banner_pc' );
 <section class="banner">
 	<?php foreach ( $banners as $banner ) : ?>
 		<div class="banner__inner">
+			<a href="<?= esc_url( $banner['link'] ) ?>">
 			<img class="banner_pc" src="<?= esc_url( wp_get_attachment_image_url( $banner['image'], 'full', false ) ); ?>" width="1920" height="454" alt="">
 			<img class="banner_mb" src="<?= esc_url( wp_get_attachment_image_url( $banner['image_mb'], 'full', false ) ); ?>" width="1920" height="454" alt="">
-			<div class="banner__wrap">
-				<div class="container">
-					<div class="banner__content">
-						<h4><?= esc_html( $banner['sub_title'] ); ?></h4>
-						<h2><?= esc_html( $banner['title'] ); ?></h2>
-						<div class="entry-content"><?= wp_kses_post( wpautop( $banner['content'] ) ); ?></div>
-						<a href="<?= esc_url( $banner['link'] ) ?>" class="buy-now">Mua luôn</a>
-					</div>
-				</div>
-			</div>
+			</a>
 		</div>
 	<?php endforeach; ?>
 </section>

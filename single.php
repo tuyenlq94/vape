7a5c33b14ababc76f983d9d3c304f82f7064eb11
tuyenlq94 <tuyenicolor@gmail.com>
2022-11-28 08:19:21 +0700

    fix trang tin tức mobile

--- single.php
+++ single.php
@@ -22,15 +22,15 @@ get_header();
 
 					get_template_part( 'template-parts/single/content', get_post_type() );
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+					// // If comments are open or we have at least one comment, load up the comment template.
+					// if ( comments_open() || get_comments_number() ) :
+					// comments_template();
+					// endif;
 
 				endwhile; // End of the loop.
 				?>
 			</div>
-			<?php vape_recent_posts();?>
+			<?php vape_recent_posts(); ?>
 		</div>
 
 	</main><!-- #main -->

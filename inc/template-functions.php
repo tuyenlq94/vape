2b3d04cfb348ab4f913b28b6a9f319dfb391020a
tuyenlq94 <tuyenicolor@gmail.com>
2022-12-07 17:40:21 +0700

    fix trang chủ

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -12,109 +12,124 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function vape_body_classes($classes)
-{
+function vape_body_classes( $classes ) {
 	// Adds a class of hfeed to non-singular pages.
-	if (!is_singular()) {
+	if ( ! is_singular() ) {
 		$classes[] = 'hfeed';
 	}
 
 	// Adds a class of no-sidebar when there is no sidebar present.
-	if (!is_active_sidebar('sidebar-1')) {
+	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 		$classes[] = 'no-sidebar';
 	}
 
 	return $classes;
 }
-add_filter('body_class', 'vape_body_classes');
+add_filter( 'body_class', 'vape_body_classes' );
 
 /**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
-function vape_pingback_header()
-{
-	if (is_singular() && pings_open()) {
-		printf('<link rel="pingback" href="%s">', esc_url(get_bloginfo('pingback_url')));
+function vape_pingback_header() {
+	if ( is_singular() && pings_open() ) {
+		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
 	}
 }
-add_action('wp_head', 'vape_pingback_header');
+add_action( 'wp_head', 'vape_pingback_header' );
 
-add_filter('woocommerce_sale_flash', 'my_custom_sale_flash', 10, 3);
-function my_custom_sale_flash($text, $post, $_product)
-{
-	$from = $_product->regular_price;
-	$to = $_product->price;
-	if ($from == $to || !$to) return '';
-	$percent = round(($from - $to) / $from * 100);
-	$text = $from > $to ? 'Giảm' : 'Tăng';
-	return '<span class="thumb-discount-label">' . $text . ' ' . $percent . ' %</span>';
+add_filter( 'woocommerce_sale_flash', 'my_custom_sale_flash', 10, 3 );
+function my_custom_sale_flash( $text, $post, $_product ) {
+	global $product;
+	if ( ! $product->is_on_sale() ) {
+		return;
+	}
+	if ( $product->is_type( 'simple' ) ) {
+		$max_percentage = ( ( $product->get_regular_price() - $product->get_sale_price() ) / $product->get_regular_price() ) * 100;
+	} elseif ( $product->is_type( 'variable' ) ) {
+		$max_percentage = 0;
+		foreach ( $product->get_children() as $child_id ) {
+			$variation = wc_get_product( $child_id );
+			$price     = $variation->get_regular_price();
+			$sale      = $variation->get_sale_price();
+			if ( $price != 0 && ! empty( $sale ) ) {
+				$percentage = ( $price - $sale ) / $price * 100;
+			}
+			if ( $percentage > $max_percentage ) {
+				$max_percentage = $percentage;
+			}
+		}
+	}
+	if ( $max_percentage > 0 ) {
+		return '<span class="thumb-discount-label">-' . round( $max_percentage ) . '%</span>';
+	}
+
 }
 
-if( ! function_exists( 'the_paginate' ) ) :
-	function the_paginate($args = null) {
+if ( ! function_exists( 'the_paginate' ) ) :
+	function the_paginate( $args = null ) {
 		$defaults = array(
-			'title'     => 'Pages',
-			'query'     => null
+			'title' => 'Pages',
+			'query' => null,
 		);
-		$args = wp_parse_args($args, $defaults);
-		extract($args, EXTR_SKIP);
-		if(is_404()) {
+		$args     = wp_parse_args( $args, $defaults );
+		extract( $args, EXTR_SKIP );
+		if ( is_404() ) {
 			return;
 		}
 		$currentPage = null;
-		$totalPage = null;
-		if(empty($query)) {
+		$totalPage   = null;
+		if ( empty( $query ) ) {
 			global $wp_query;
 			$query = $wp_query;
 		}
 		$currentPage = stheme_get_paged();
-		$title .= ':';
-		$ppp = intval($query->query['posts_per_page']);
-		if($ppp < 1) {
+		$title      .= ':';
+		$ppp         = intval( $query->query['posts_per_page'] );
+		if ( $ppp < 1 ) {
 			return;
 		}
-		$totalPage = intval(ceil($query->found_posts / $ppp));
-		if($totalPage <= 1) {
+		$totalPage = intval( ceil( $query->found_posts / $ppp ) );
+		if ( $totalPage <= 1 ) {
 			return;
 		}
-		$paginateResult = '<div id="sPaginate" class="spaginate paginate"><div class="spaginate-inner paginate-inner"><span class="spaginate-title paginate-title">'.$title.'</span>';
-		if ($currentPage > 1) {
-			$paginateResult .= '<a class="spaginate-prev prev page-item" href="'.get_pagenum_link($currentPage - 1).'">&laquo;</a>';
+		$paginateResult = '<div id="sPaginate" class="spaginate paginate"><div class="spaginate-inner paginate-inner"><span class="spaginate-title paginate-title">' . $title . '</span>';
+		if ( $currentPage > 1 ) {
+			$paginateResult .= '<a class="spaginate-prev prev page-item" href="' . get_pagenum_link( $currentPage - 1 ) . '">&laquo;</a>';
 		}
-		$paginateResult .= the_paginate_list(1, $totalPage, $currentPage);
-		if ($currentPage < $totalPage) {
-			$paginateResult .= "<a href='" . get_pagenum_link($currentPage + 1) . "' class='spaginate-next next page-item'>&raquo;</a>";
+		$paginateResult .= the_paginate_list( 1, $totalPage, $currentPage );
+		if ( $currentPage < $totalPage ) {
+			$paginateResult .= "<a href='" . get_pagenum_link( $currentPage + 1 ) . "' class='spaginate-next next page-item'>&raquo;</a>";
 		}
-		$paginateResult .= "</div></div>"; 
+		$paginateResult .= '</div></div>';
 		echo $paginateResult;
 	}
 endif;
 
-if( ! function_exists( 'stheme_paginate' ) ) :
-	function stheme_paginate($args = null) {
-		the_paginate($args);
+if ( ! function_exists( 'stheme_paginate' ) ) :
+	function stheme_paginate( $args = null ) {
+		the_paginate( $args );
 	}
 endif;
 
-if( ! function_exists( 'the_paginate_list' ) ) :
-	function the_paginate_list($intStart, $totalPage, $currentPage) {
-		$pageHidden = '<span class="spaginate-hidden hidden">...</span>';
-		$linkResult = "";
+if ( ! function_exists( 'the_paginate_list' ) ) :
+	function the_paginate_list( $intStart, $totalPage, $currentPage ) {
+		$pageHidden   = '<span class="spaginate-hidden hidden">...</span>';
+		$linkResult   = '';
 		$hiddenBefore = false;
-		$hiddenAfter = false;
-		for ($i = $intStart; $i <= $totalPage; $i++) {
-			if($currentPage === intval($i)) {
-				$linkResult .= '<span class="spaginate-current page-item current">'.$i.'</span>';
-			} elseif(($i <= 6 && $currentPage < 10) || $i == $totalPage || $i == 1 || $i < 4 || ($i <= 6 && $totalPage <= 6) || ($i > $currentPage && ($i <= ($currentPage + 2))) || ($i < $currentPage && ($i >= ($currentPage - 2))) || ($i >= ($totalPage - 2) && $i < $totalPage)) {
-				$linkResult .= '<a class="spaginate-link page-item" href="'.get_pagenum_link($i).'">'.$i.'</a>';
-				if($i <= 6 && $currentPage < 10) {
+		$hiddenAfter  = false;
+		for ( $i = $intStart; $i <= $totalPage; $i++ ) {
+			if ( $currentPage === intval( $i ) ) {
+				$linkResult .= '<span class="spaginate-current page-item current">' . $i . '</span>';
+			} elseif ( ( $i <= 6 && $currentPage < 10 ) || $i == $totalPage || $i == 1 || $i < 4 || ( $i <= 6 && $totalPage <= 6 ) || ( $i > $currentPage && ( $i <= ( $currentPage + 2 ) ) ) || ( $i < $currentPage && ( $i >= ( $currentPage - 2 ) ) ) || ( $i >= ( $totalPage - 2 ) && $i < $totalPage ) ) {
+				$linkResult .= '<a class="spaginate-link page-item" href="' . get_pagenum_link( $i ) . '">' . $i . '</a>';
+				if ( $i <= 6 && $currentPage < 10 ) {
 					$hiddenBefore = true;
 				}
 			} else {
-				if(!$hiddenBefore) {
-					$linkResult .= $pageHidden;
+				if ( ! $hiddenBefore ) {
+					$linkResult  .= $pageHidden;
 					$hiddenBefore = true;
-				} elseif(!$hiddenAfter && $i > $currentPage) {
+				} elseif ( ! $hiddenAfter && $i > $currentPage ) {
 					$linkResult .= $pageHidden;
 					$hiddenAfter = true;
 				}
@@ -125,6 +140,6 @@ if( ! function_exists( 'the_paginate_list' ) ) :
 endif;
 
 function stheme_get_paged() {
-	$paged = intval(get_query_var('paged')) ? intval(get_query_var('paged')) : 1;
+	$paged = intval( get_query_var( 'paged' ) ) ? intval( get_query_var( 'paged' ) ) : 1;
 	return $paged;
 }
--- template-parts/home/featured-products.php
+++ template-parts/home/featured-products.php
@@ -1,28 +1,22 @@
 <?php
-$tax_query[] = array(
-	'taxonomy' => 'product_visibility',
-	'field'    => 'name',
-	'terms'    => 'featured',
-	'operator' => 'IN',
-);
-$args = array(
-	'post_type' => 'product',
+$product_featured = rwmb_meta( 'product_featured' );
+
+$args  = array(
+	'post_type'   => 'product',
 	'post_status' => 'publish',
-	'posts_per_page' => 10,
-	'ignore_sticky_posts' => 1,
-	'tax_query' => $tax_query,
+	'post__in'    => $product_featured,
 );
-$query = new WP_query($args);
+$query = new WP_query( $args );
 ?>
 <section class="featured">
 	<div class="container">
 		<div class="featured__top">
-			<h2 class="title"><?= esc_html__('Vape hàng hiệu gợi ý cho bạn', 'vape') ?></h2>
+			<h2 class="title"><?= esc_html__( 'Vape hàng hiệu gợi ý cho bạn', 'vape' ) ?></h2>
 		</div>
 		<div class="featured__wrap">
 			<?php
-			if ($query->have_posts()) :
-				while ($query->have_posts()) :
+			if ( $query->have_posts() ) :
+				while ( $query->have_posts() ) :
 					$query->the_post();
 					echo '<div class="featured__item">';
 					vape_products();
